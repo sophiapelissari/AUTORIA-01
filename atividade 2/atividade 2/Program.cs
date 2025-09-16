2e1c72bb59adb1c26790ca73b08de5024bb0cf40
sophia <sophiasecundaria04@gmail.com>
2025-09-16 12:10:46 -0300

    feat: atividade 2

---
+++ atividade 2/atividade 2/Program.cs
@@ -0,0 +1,12 @@
+﻿int temp;
+Console.WriteLine("Digite a temperatura do dia (em graus Celsius): ");
+float temperatura = float.Parse(Console.ReadLine());
+//lendo a temperatura dada 
+if (temperatura < 20 || temperatura > 30)
+{
+    Console.WriteLine("Temperatura fora da faixa confortável! ;(");
+}
+else
+{
+    Console.WriteLine("Temperatura confortável ;)");
+}
