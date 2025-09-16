37430de5c8e2f14dca3ea6b4c9704d1c2f41c00b
sophia <sophiasecundaria04@gmail.com>
2025-09-16 12:13:52 -0300

    feat: atividade 3

---
+++ atividade 3/atividade 3/Program.cs
@@ -0,0 +1,11 @@
+﻿double velo;
+Console.WriteLine("Digite a distância percorrida em quilômetros:");
+double distancia = double.Parse(Console.ReadLine());// Verifica se a distância é diferente de 60
+if (distancia != 60)
+{
+    Console.WriteLine("Distância fora do padrão esperado. :(");
+}
+else
+{
+    Console.WriteLine("Distância dentro do padrão esperado. :)");
+}
